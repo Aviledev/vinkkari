eb6551417693389ce06413b1236f51fd0d892e5a
Leevi Lehtonen <leevi.k.lehtonen@helsinki.fi>
2017-11-19 21:44:04 +0200

    fixing for cucumber

--- src/test/java/avile/domain/BookRecommendationTest.java
+++ src/test/java/avile/domain/BookRecommendationTest.java
@@ -9,7 +9,7 @@ import static org.junit.Assert.*;
 
 public class BookRecommendationTest {
 
-    BookRecommendation bookRecommendation;
+    private BookRecommendation bookRecommendation;
 
     @Before
     public void setUp() throws Exception {
