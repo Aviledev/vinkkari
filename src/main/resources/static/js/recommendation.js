7ccd6244c91bad3c1420356539ee1a0d6dfd9691
Leevi Lehtonen <leevi.k.lehtonen@helsinki.fi>
2017-11-19 21:43:18 +0200

    fixing for cucumber

--- src/main/resources/static/js/recommendation.js
+++ src/main/resources/static/js/recommendation.js
@@ -1,5 +1,5 @@
-$('#inputBookTags').tagsinput({
-    tagClass: 'h4 badge badge-primary mb-0',
+$("#inputBookTags").tagsinput({
+    tagClass: "h4 badge badge-primary mb-0",
     trimValue: true,
     maxChars: 24,
     maxTags: 32,
@@ -7,6 +7,6 @@ $('#inputBookTags').tagsinput({
 });
 
 
-$('#submitBtn').click(function () {
-    $('#create'+$('.nav-pills .active').text()+ 'Form').submit();
+$("#submitBtn").click(function () {
+    $("#create"+$(".nav-pills .active").text()+ "Form").submit();
 });
--- src/test/java/avile/Stepdefs.java
+++ src/test/java/avile/Stepdefs.java
@@ -1,8 +1,6 @@
 package avile;
 
-import cucumber.api.PendingException;
 import cucumber.api.java.After;
-import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
