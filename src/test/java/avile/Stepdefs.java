4d6505ef180829e054546f7081f0b4ee08848fbb
alehuo <aleksi.huotala@helsinki.fi>
2017-11-22 16:11:06 +0200

    Added page source print for debug purposes

--- src/test/java/avile/Stepdefs.java
+++ src/test/java/avile/Stepdefs.java
@@ -164,8 +164,10 @@ public class Stepdefs {
 
     @And("^book with title \"([^\"]*)\" is found$")
     public void bookWithTitleIsFound(String arg0) throws Throwable {
+        System.out.println(driver.getPageSource());
         new WebDriverWait(driver, 10)
                 .until(ExpectedConditions.titleIs("VINKKARI | Search results"));
+        System.out.println(driver.getPageSource());
         assertTrue(driver.getPageSource().contains(arg0));
     }
 }
