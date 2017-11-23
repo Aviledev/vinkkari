91b375b9472b811c6c04203844bf8729e883702c
alehuo <aleksi.huotala@helsinki.fi>
2017-11-23 14:10:53 +0200

    Add Entity annotation

--- src/main/java/avile/domain/VideoRecommendation.java
+++ src/main/java/avile/domain/VideoRecommendation.java
@@ -2,6 +2,9 @@ package avile.domain;
 
 import org.springframework.data.jpa.domain.AbstractPersistable;
 
+import javax.persistence.Entity;
+
+@Entity
 public class VideoRecommendation extends AbstractPersistable<Long>
 {
     private String url;
