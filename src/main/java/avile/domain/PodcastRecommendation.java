17c329ad33cf87a44f3fe2c35d7dec41fc7737c0
teepiik <teemu.piik@gmail.com>
2017-11-23 11:20:15 +0200

    podcast validations, crud

--- src/main/java/avile/domain/PodcastRecommendation.java
+++ src/main/java/avile/domain/PodcastRecommendation.java
@@ -4,15 +4,28 @@ import java.util.Date;
 import javax.persistence.Entity;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import org.springframework.data.jpa.domain.AbstractPersistable;
 
 @Entity
 public class PodcastRecommendation extends AbstractPersistable<Long>{
-    // @Max(20)
+    
+    @NotNull
+    @Size(min=2, max=50)
     private String title;
-    private String writer;
+    
+    @Max(40)
+    private String author;
+    
+    @NotNull
     private String type;
+    
+    @NotNull
     private String url;
+    
+    @Max(300)
     private String description;
     //private List<String> tags;
     //private List<String> prerequisiteCourses;
@@ -20,4 +33,58 @@ public class PodcastRecommendation extends AbstractPersistable<Long>{
     
     @Temporal(TemporalType.TIMESTAMP)
     private Date date;
+
+    public PodcastRecommendation() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+    
+    
+    
 }
