c3a212d5cababb41e62f7e3bce50ab7e086995cd
Leevi Lehtonen <leevi.k.lehtonen@helsinki.fi>
2017-12-07 07:36:34 +0200

    Tests, sample data
    -Completed the missing tests
    -Added real data as sample data
    -Added course data

--- src/test/java/avile/domain/CourseTest.java
+++ src/test/java/avile/domain/CourseTest.java
@@ -1,8 +1,9 @@
 package avile.domain;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class CourseTest {
     private Course course;
@@ -31,6 +32,13 @@ public class CourseTest {
         String desc = "Lorem ipsum dolor sit amet";
         course.setDescription(desc);
         assertEquals(desc, course.getDescription());
+    }
+
+    @Test
+    public void getSetId() throws Exception {
+        Long id = 123456789L;
+        course.setId(id);
+        assertEquals(id, course.getId());
     }
 
 }
--- src/test/java/avile/domain/TagTest.java
+++ src/test/java/avile/domain/TagTest.java
@@ -1,15 +1,15 @@
 package avile.domain;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
 public class TagTest {
     private Tag tag;
-    
-    public TagTest() {
-    }
+
       
     @Before
     public void setUp() {
@@ -21,6 +21,25 @@ public class TagTest {
         String tagName = "easy";
         tag.setName(tagName);
         assertEquals("easy", tag.getName());
+    }
+
+    @Test
+    public void getSetDescription() {
+        String tagDesc = "easy";
+        tag.setDescription(tagDesc);
+        assertEquals("easy", tag.getDescription());
+    }
+
+    @Test
+    public void getSetRecommendations() {
+        tag.setRecommendations(new ArrayList<>());
+        assertEquals(0, tag.getRecommendations().size());
+        Recommendation recommendation = new Recommendation();
+        tag.getRecommendations().add(recommendation);
+        assertEquals(1, tag.getRecommendations().size());
+        tag.getRecommendations().remove(recommendation);
+        assertEquals(0, tag.getRecommendations().size());
+
     }
    
 }
