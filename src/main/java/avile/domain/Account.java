7da2cf243e2a2e0736e2c31b86016948c3500099
Leevi Lehtonen <leevi.k.lehtonen@helsinki.fi>
2017-12-09 20:19:11 +0200

    Account model created

---
+++ src/main/java/avile/domain/Account.java
@@ -0,0 +1,58 @@
+package avile.domain;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotBlank;
+import org.springframework.data.jpa.domain.AbstractPersistable;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Account extends AbstractPersistable<Long> {
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @Length(max = 100)
+    private String firstname;
+
+    @Length(max = 100)
+    private String lastname;
+
+    @NotBlank
+    @Length(max = 200)
+    private String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
