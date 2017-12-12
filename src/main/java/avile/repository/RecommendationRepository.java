242411bd0c8b70326950739e04a9d71d00cf34c5
alehuo <aleksi.huotala@helsinki.fi>
2017-12-12 21:04:05 +0200

    Added filtering

--- src/main/java/avile/repository/RecommendationRepository.java
+++ src/main/java/avile/repository/RecommendationRepository.java
@@ -2,12 +2,18 @@ package avile.repository;
 
 import avile.domain.Account;
 import avile.domain.Recommendation;
+import avile.enums.RecommendationType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface RecommendationRepository extends JpaRepository<Recommendation, Long>{
+public interface RecommendationRepository extends JpaRepository<Recommendation, Long> {
 
     List<Recommendation> findRecommendationsByTitleIsLikeIgnoreCase(String title);
+
     List<Recommendation> findRecommendationsByCheckersNotContaining(Account account);
+
+    List<Recommendation> findRecommendationsByRecommendationTypeAndCheckersNotContaining(RecommendationType recommendationType, Account account);
+
+    List<Recommendation> findRecommendationsByRecommendationType(RecommendationType recommendationType);
 }
